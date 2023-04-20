8ff668be971a1d6eead772e269e7924a8dc4aac8
Mathan22-devops <78956959+Mathan22-devops@users.noreply.github.com>
2023-04-20 11:46:30 +0530

    hpi
    added

--- AppController.java
+++ AppController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
-	@Autowired
+	@Autowired hpi
 	private UserRepository repo;
 	    @GetMapping("")
 	    public String viewHomePage() {
